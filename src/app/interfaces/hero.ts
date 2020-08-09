7024b3c1f65019c6866f109ce7857fbe7ed8cc1b
Rafael Mesquita <rafaelempresarialacc@gmail.com>
2020-08-09 01:05:33 -0300

    Interface de herói adicionada

---
+++ src/app/interfaces/hero.ts
@@ -0,0 +1,18 @@
+export interface Hero{
+  //overview
+  id : number;
+  name : string;
+  role : string;
+  difficulty : number;
+  skills : [{
+    name : string;
+    description : string;
+  }]
+
+  //biography
+  real_name : string;
+  occupation : string;
+  base_of_operations : string;
+  affiliation : string;
+  history: string;
+}
